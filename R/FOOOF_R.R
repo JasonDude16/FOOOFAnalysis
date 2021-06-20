fc1c887c5036360efd59754e8f791013b60639fc
Jason Dude <jasondude16@gmail.com>
2021-06-19 22:11:56 -0400

    Added FOOOF wrapper

---
+++ R/FOOOF_R.R
@@ -0,0 +1,25 @@
+#' Wrapper for FOOOF module in Python
+#'
+#'
+#' @param data_list data_list
+#' @param append append
+#' @param img_path img_path
+#' @param data_path data_path
+#' @param freq_range freq_range
+#' @export
+#' @example
+
+FOOOF_R <- function(data_list, append = NULL, img_path = NULL, data_path = NULL, freq_range = c(1L, 50L)) {
+
+    if (!require(reticulate))
+        message("`reticulate` is required to use the FOOOF_R function")
+
+    py$FOOOF_R(
+        data_list = data_list,
+        append = append,
+        img_path = img_path,
+        data_path = data_path,
+        freq_range = freq_range
+    )
+
+}
---
+++ inst/python/FOOOF_R.py
@@ -0,0 +1,32 @@
+import numpy as np
+import fooof
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def FOOOF_R (data_list, append=None, img_path=None, data_path=None, freq_range=[1,50]):
+  
+  df = []
+  ID_list = list(data_list.keys())
+  
+  for ID in ID_list:
+    spectrum = np.ravel(data_list[ID]['spectrum'])
+    freqs = np.ravel(data_list[ID]['freqs'])
+    fm = fooof.FOOOF(peak_width_limits= [1, 8], max_n_peaks= 8, min_peak_height= 0.05)
+    fm.report(freqs, spectrum, freq_range = freq_range)
+    plt.close()
+    df.append([ID,
+               fm.aperiodic_params_.round(decimals=3)[1],
+               fm.r_squared_.round(decimals = 3),
+               fm.error_.round(decimals = 3)])
+    if img_path is not None:
+      file = ID + "_" + append + '.png'
+      fm.plot(save_fig= True, file_name=file, file_path=img_path)
+      plt.close()
+
+  df = pd.DataFrame(df, columns=['ID', 'aperiodic_exp', 'r_squared', 'model_error'])
+  
+  if data_path is not None: 
+      file = data_path + "/models_" + append + ".csv"
+      df.to_csv(file)
+      
+  return df 
